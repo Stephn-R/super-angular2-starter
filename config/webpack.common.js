d5f8ae508ec3466b209fb30346afda6b1f2ecc82
Stephen Rodriguez <steprodriguez10@gmail.com>
2016-08-22 17:07:59 -0400

    barebones app using webpack

---
+++ config/webpack.common.js
@@ -0,0 +1,66 @@
+'use strict';
+
+/*=============================================>>>>>
+= MODULES =
+===============================================>>>>>*/
+
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const autoprefixer = require('autoprefixer');
+
+/*= End of MODULES =*/
+/*=============================================<<<<<*/
+
+const METADATA = {
+  title:       'SAS: Super Angular2 Starter',
+  ENV:         (process.env.NODE_ENV)
+};
+
+const config = {
+  metadata:       METADATA,
+  entry:          {
+    main: [ './main.ts' ]
+  },
+  output: {
+    path:       './bin',
+    publicPath: '/',
+    filename:   '[name].bundle.js'
+  },
+  resolve: {
+    root:               '.',
+    extensions:         [ '', '.js', '.ts' ],
+    modulesDirectories: [ 'node_modules' ]
+  },
+  module: {
+    loaders: [
+      { test: /\.(sass|scss)$/, exclude: /(node_modules)/, loaders: [ 'style', 'postcss', 'sass?sourceMap' ] },
+      { test: /\.(png|jpg|jpeg)$/, loader: 'file?name=[name].[ext]' },
+      { test: /\.html$/, loader: 'html' }
+    ]
+  },
+  node: {
+    global:         'window',
+    crypto:         'empty',
+    module:         false,
+    clearImmediate: false,
+    setImmediate:   false
+  },
+  postcss: function() {
+    return [ autoprefixer ];
+  },
+  plugins: [
+    new webpack.optimize.OccurenceOrderPlugin(true),
+    new webpack.optimize.DedupePlugin(),
+    new HtmlWebpackPlugin({
+      showError:      true,
+      template:       './index.ejs',
+      filename:       'index.html',
+      chunksSortMode: 'dependency'
+    }),
+    new webpack.EnvironmentPlugin([
+      'NODE_ENV'
+    ])
+  ]
+};
+
+module.exports = config;
---
+++ config/webpack.prod.js
@@ -0,0 +1,48 @@
+/* eslint no-magic-numbers:0 */
+'use strict';
+
+/*=============================================>>>>>
+= MODULES =
+===============================================>>>>>*/
+
+const path = require('path');
+const merge = require('webpack-merge');
+const common = require('./webpack.common');
+
+const webpack = require('webpack');
+const WebpackMd5Hash = require('webpack-md5-hash');
+const WebpackNotifierPlugin = require('webpack-notifier');
+
+/*= End of MODULES =*/
+/*=============================================<<<<<*/
+
+module.exports = merge(common, {
+  debug:   false,
+  output:  {
+    path:              path.resolve('${__dirname}/../dist'),
+    filename:          '[name].[chunkhash].bundle.js',
+    sourceMapFilename: '[name].[chunkhash].bundle.map',
+    chunkFilename:     '[id].[chunkhash].chunk.js',
+    library:           'ac_[name]',
+    libraryTarget:     'var'
+  },
+  plugins: [
+    new WebpackMd5Hash(),
+    new WebpackNotifierPlugin({
+      title:        'SAS: Application',
+      alwaysNotify: true
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress:  { warnings: false },
+      mangle:    false,
+      comments:  false,
+      sourceMap: false,
+      exclude:   [ ]
+    })
+  ],
+  htmlLoader: {
+    minimize:              true,
+    removeAttributeQuotes: false,
+    caseSensitive:         true
+  }
+});
